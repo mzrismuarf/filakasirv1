61c62faf8f6442eb117de20c5498da42c1b0fbfc
mzrismuarf <mzrismuarf@gmail.com>
2024-07-26 15:39:51 +0700

    Add search in the column

--- app/Filament/Resources/PelangganResource.php
+++ app/Filament/Resources/PelangganResource.php
@@ -40,9 +40,9 @@ class PelangganResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('nama'),
-                Tables\Columns\TextColumn::make('no_telepon')->label('Nomer Telepon'),
-                Tables\Columns\TextColumn::make('alamat'),
+                Tables\Columns\TextColumn::make('nama')->searchable(),
+                Tables\Columns\TextColumn::make('no_telepon')->label('Nomer Telepon')->searchable(),
+                Tables\Columns\TextColumn::make('alamat')->searchable(),
             ])
             ->filters([
                 //
