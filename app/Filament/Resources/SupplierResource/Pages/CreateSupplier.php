352f8d70153c166bed440e219c8b1d907fb60440
mzrismuarf <mzrismuarf@gmail.com>
2024-07-30 12:40:07 +0700

    Add Menu Data Supplier

---
+++ app/Filament/Resources/SupplierResource/Pages/CreateSupplier.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\SupplierResource\Pages;
+
+use App\Filament\Resources\SupplierResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSupplier extends CreateRecord
+{
+    protected static string $resource = SupplierResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Data Supplier berhasil ditambahkan';
+    }
+}
---
+++ app/Filament/Resources/SupplierResource/Pages/EditSupplier.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\SupplierResource\Pages;
+
+use App\Filament\Resources\SupplierResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSupplier extends EditRecord
+{
+    protected static string $resource = SupplierResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Data Supplier berhasil diperbarui';
+    }
+}
