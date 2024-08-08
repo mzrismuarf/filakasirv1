4280c54750fb2c83e9d7bf0c7daa4e75e3d67f98
mzrismuarf <mzrismuarf@gmail.com>
2024-08-08 14:02:19 +0700

    fix: adding a price column

--- database/migrations/2024_07_31_064315_create_barangs_table.php
+++ database/migrations/2024_07_31_064315_create_barangs_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('kategori')->nullable();
             $table->string('stok');
             $table->string('satuan');
+            $table->integer('harga')->default(0);
             $table->timestamps();
         });
     }
