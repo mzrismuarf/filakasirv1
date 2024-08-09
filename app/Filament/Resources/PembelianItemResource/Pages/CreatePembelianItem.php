48a9784131f16a30761e23273b1553932e97686b
mzrismuarf <mzrismuarf@gmail.com>
2024-08-09 10:27:34 +0700

    fix(item purchase): fix form reset issue in CreatePembelianItem by adding actions and redirect

--- app/Filament/Resources/PembelianItemResource/Pages/CreatePembelianItem.php
+++ app/Filament/Resources/PembelianItemResource/Pages/CreatePembelianItem.php
@@ -2,11 +2,33 @@
 
 namespace App\Filament\Resources\PembelianItemResource\Pages;
 
-use App\Filament\Resources\PembelianItemResource;
-use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\PembelianItemResource;
 
 class CreatePembelianItem extends CreateRecord
 {
     protected static string $resource = PembelianItemResource::class;
+
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('create')
+                ->label(__('Simpan'))
+                ->submit('create')
+                ->keyBindings(['mod+s']),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        $id = $this->record->pembelian_id;
+        return route(
+            'filament.admin.resources.pembelian-items.create',
+            [
+                'pembelian_id' => $id
+            ]
+        );
+    }
 }
