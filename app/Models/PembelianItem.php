4a1e43ee76c79d1811bdbf56f2ea3cd499838c0d
mzrismuarf <mzrismuarf@gmail.com>
2024-08-09 10:40:36 +0700

    Add item relationships to the PurchaseItem model

--- app/Models/PembelianItem.php
+++ app/Models/PembelianItem.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PembelianItem extends Model
 {
     use HasFactory;
+
+    // membuat relasi dari pembelian item ke barang (barang_id)
+    /**
+     * Get the barang that owns the PembelianItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function barang(): BelongsTo
+    {
+        return $this->belongsTo(Barang::class);
+    }
 }
